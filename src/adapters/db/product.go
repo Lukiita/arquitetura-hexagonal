49ba8e7b8a83fcffd2270bdbfb5fc67b78301818
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-04-05 16:55:45 -0300

    feat: add database

---
+++ src/adapters/db/product.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/Lukiita/go-hexagonal/src/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (p *ProductDb) Get(id string) (application.IProduct, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
