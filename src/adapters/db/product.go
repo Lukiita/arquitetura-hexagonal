ed88a48390186a6071d67751f12627f3f1b343b2
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-04-06 09:46:01 -0300

    feat: add product service

--- src/adapters/db/product.go
+++ src/adapters/db/product.go
@@ -11,6 +11,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db}
+}
+
 func (p *ProductDb) Get(id string) (application.IProduct, error) {
 	var product application.Product
 	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
@@ -23,4 +27,64 @@ func (p *ProductDb) Get(id string) (application.IProduct, error) {
 	}
 
 	return &product, nil
+}
+
+func (p *ProductDb) Save(product application.IProduct) (application.IProduct, error) {
+	var rows int
+	p.db.QueryRow("SELECT id FROM products WHERE id = ?", product.GetID()).Scan(&rows)
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) create(product application.IProduct) (application.IProduct, error) {
+
+	stmt, err := p.db.Prepare(`INSERT INTO products(id, name, price, status) VALUES (?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		product.GetID(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.IProduct) (application.IProduct, error) {
+	_, err := p.db.Exec(
+		"UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?",
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+		product.GetStatus(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
 }
